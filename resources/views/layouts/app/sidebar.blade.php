fab01ef65eff89d4fef2044eb72d04f6f6778357
Dicky03a <dickycoding@gmail.com>
2026-02-15 21:30:52 +0700

    Perubahan Logo Sidebar

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -8,7 +8,14 @@
 <body class="min-h-screen bg-white dark:bg-zinc-800">
     <flux:sidebar sticky collapsible="mobile" class="border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
         <flux:sidebar.header>
-            <x-app-logo :sidebar="true" href="{{ route('dashboard') }}" wire:navigate />
+            <!-- <x-app-logo :sidebar="true" href="{{ route('dashboard') }}" wire:navigate /> -->
+            <div class="flex items-center gap-1">
+                <img src="{{ asset('asset/laziznulogo.svg') }}" alt="Logo NU Care" class="h-16 w-auto">
+                <div class="">
+                    <h2 class="font-bold text-green-800">Laziznu</h2>
+                    <h2 class="font-bold text-green-800 -mt-1">Bojonegoro</h2>
+                </div>
+            </div>
             <flux:sidebar.collapse class="lg:hidden" />
         </flux:sidebar.header>
 
